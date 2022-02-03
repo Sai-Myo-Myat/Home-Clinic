14c355b8934375aa913bcaa7257b98ce8c9dc6eb
Sai-Myo-Myat <saimyomyat511@gmail.com>
2022-02-03 22:49:12 +0630

    first

---
+++ components/Main.js
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <div className="h-[100%] bg-blue-700"> hello</div>;
+};
+
+export default Main;
---
+++ components/Nav.js
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <nav className="bg-blue-500 text-white text-wihte flex justify-around">
+      <h1 className="py-3 ">Home</h1>
+      <ul className="py-3 ">
+        <li> Q&A</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
