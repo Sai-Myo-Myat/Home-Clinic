0f509a62ac669a4a36b658c22a1d887dafacddcf
Sai-Myo-Myat <saimyomyat511@gmail.com>
2022-02-04 23:02:21 +0630

    3th

--- components/Stepper.js
+++ components/Stepper.js
@@ -3,33 +3,33 @@ import "tw-elements";
 const Stepper = () => {
   return (
     <div>
-      <ul class="stepper" data-mdb-stepper="stepper">
-        <li className="stepper-step stepper-active">
-          <div className="stepper-head">
-            <span class="stepper-head-icon"> 1 </span>
-            <span class="stepper-head-text"> step1 </span>
+      <ul className="stepper" data-mdb-stepper="stepper">
+        <li classNameName="stepper-step stepper-active">
+          <div classNameName="stepper-head">
+            <span className="stepper-head-icon"> 1 </span>
+            <span className="stepper-head-text"> step1 </span>
           </div>
-          <div class="stepper-content">
+          <div className="stepper-content">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </div>
         </li>
-        <li class="stepper-step">
-          <div class="stepper-head">
-            <span class="stepper-head-icon"> 2 </span>
-            <span class="stepper-head-text"> step2 </span>
+        <li className="stepper-step">
+          <div className="stepper-head">
+            <span className="stepper-head-icon"> 2 </span>
+            <span className="stepper-head-text"> step2 </span>
           </div>
-          <div class="stepper-content">
+          <div className="stepper-content">
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat.
           </div>
         </li>
-        <li class="stepper-step">
-          <div class="stepper-head">
-            <span class="stepper-head-icon"> 3 </span>
-            <span class="stepper-head-text"> step3 </span>
+        <li className="stepper-step">
+          <div className="stepper-head">
+            <span className="stepper-head-icon"> 3 </span>
+            <span className="stepper-head-text"> step3 </span>
           </div>
-          <div class="stepper-content">
+          <div className="stepper-content">
             Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur.
           </div>
